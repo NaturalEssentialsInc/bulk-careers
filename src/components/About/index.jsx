3c782c66a3bcc9249a321aa19b1b1703899c3625
carlumbo <carlpalumbo1@gmail.com>
2022-09-20 14:10:16 -0400

    updated landing page info

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,17 +9,17 @@ const About = () => {
       <h2>About us</h2>
       <div>
         <p>
-          Bulk Apothecary was founded in mid-2010 as a small brick & mortar
-          specialty retailer in Streetsboro, Ohio, and has since grown to become
-          one of the nation's largest online suppliers of natural ingredients
-          and essential oils. Our primary focus from the beginning was soap and
-          candle-making supplies, but the word of our fantastic prices, amazing
-          quality, and excellent customer service spread. Within three years, we
-          expanded our offering to a wide range of specialty ingredients and
-          vitamin supplements. We had a dominant online presence that was
-          growing by the day. Today, we still operate our small store in Aurora,
-          Ohio but also have an online presence that's growth is nothing short
-          of amazing.
+        Natural Essentials, Inc. was founded in 1995 as a family-owned business in Aurora, Ohio. Since then, the company has grown from humble beginnings into one of the nation’s top contract filling companies. 
+
+ 
+
+        In 2010, the family launched Bulk Apothecary as a way to expand Natural Essentials. Originally a brick and mortar store in Streetsboro, Bulk Apothecary has also grown rapidly from its grassroots into one of the nation’s largest online retailers of natural ingredients, essential oils, soap making, and candle making supplies. Our main focus at Bulk Apothecary is providing the best possible customer service by offering amazing products at great prices and committing daily to positively impact our customers’ experience. 
+
+ 
+
+        As a family-owned business, our owners ensure that our employees are able to spend as much time with their families as possible. Maintaining a healthy work-life balance is highly valued here. We only operate Monday-Friday so that parents have weekends with their children, provide amazing healthcare and benefits options for all types of families, and, through a team-oriented atmosphere, promote a positive work environment. 
+
+        Employees in all departments have fun, work together, and get the job done!
         </p>
         <div className={wrapper}>
           <StaticImage
@@ -30,17 +30,7 @@ const About = () => {
             placeholder="blurred"
             src="../../images/bulk-logo.png"
           />
-        </div>
-        <p>
-          Natural Essentials was founded in 1995 and has quickly grown to one of
-          the nation's premier contract-filling companies. Because of our strong
-          focus on quality, customer service, and price, Natural Essentials has
-          maintained outstanding growth in one of the worst economies in U.S.
-          history. Today, Natural Essentials employs nearly 275 employees and
-          manufactures products found in almost every retailer in the USA.
-        </p>
-        <div className={wrapper2}>
-          <StaticImage
+           <StaticImage
             alt="Natural Essentials Inc"
             height={60}
             layout="constrained"
